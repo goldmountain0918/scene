732e14bd1a9a18260dfcec1dd6f8d1c0a11a8002
goldmountain0918 <goldmountain0918@gmail.com>
2020-08-21 22:12:54 +0800

    fix scene1_new

--- scene1_new/assets/js/script.js
+++ scene1_new/assets/js/script.js
@@ -9,6 +9,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         document.getElementById('video').play();
         document.getElementById('main').classList.add('inactive');
     });
+    document.getElementById('back').addEventListener('click', () => {
+        document.getElementById('video').classList.remove('active');
+        document.getElementById('main').classList.remove('inactive');
+        setTimeout(function() {
+            calling();
+        }, 5000);
+    });
 });
 
 
