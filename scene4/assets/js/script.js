1474c707e147749d42ecd27a4e324a7e2ab6ec5f
goldmountain0918 <goldmountain0918@gmail.com>
2020-08-25 01:38:09 +0800

    scene 4 gear icon fix

--- scene4/assets/js/script.js
+++ scene4/assets/js/script.js
@@ -17,32 +17,21 @@ $(function() {
     var timer;
     var prompt;
     var string;
+    var errorFlag = true;
     var term = $('#terminal').terminal(function(command) {
         if (command !== '') {
-            var command = command;
-/*            var i = 0, size = 100;
-            prompt = term.get_prompt();
-            string = progress(0, size);
-            term.set_prompt(progress);
-            animation = true;
-            (function loop() {
-                string = progress(i++, size);
-                term.set_prompt(string);
-                if (i < 100) {
-                    timer = setTimeout(loop, 100);
-                } else {
-                    term.echo(progress(i, size) + ' [[b;green;]OK]')
-                        .set_prompt(prompt);
-                    animation = false
-                }
-            })();*/
             var i = 1;
             (function loop() {
                 term.echo(`complex code line ${i++}`);
                 if (i < 31) {
                     timer = setTimeout(loop, 100);
                 } else {
-                    term.echo(' [[b;green;]OK]')
+                    if (errorFlag) {
+                        term.echo(' [[b;red;]ERROR]');
+                    } else {
+                        term.echo(' [[b;green;]OK]');
+                    }
+                    errorFlag = !errorFlag;
                     animation = false
                 }
             })();
@@ -54,7 +43,4 @@ $(function() {
             name: '',
             height: 583
         });
-    $('#gear').on('click', function () {
-        term.exec(command);
-    })
 });
