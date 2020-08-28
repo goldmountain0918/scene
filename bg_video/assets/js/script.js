187d261a37d93cacce5ba52010380d8ce2345294
goldmountain0918 <goldmountain0918@gmail.com>
2020-08-29 00:11:00 +0800

    bg_video fix flashing

--- bg_video/assets/js/script.js
+++ bg_video/assets/js/script.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var i = 1;
-    var video1 = document.getElementById('video1');
-    var video2 = document.getElementById('video2');
-    var video3 = document.getElementById('video3');
     document.addEventListener('keypress', () => {
-        video1.style.display = 'none';
-        video2.style.display = 'none';
-        video3.style.display = 'none';
-        document.getElementById(`video${++i}`).style.display = 'block';
-        if (i === 3) i = 0;
+        if (i < 3) {
+            i++;
+            document.getElementById(`video${i}`).style['z-index'] = i;
+            document.getElementById(`video${i}`).play();
+        }
     });
 });
