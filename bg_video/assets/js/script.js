39d90f4a33a10d8be1a74be2c24f9e2fe0b5de6a
goldmountain0918 <goldmountain0918@gmail.com>
2020-08-29 00:24:06 +0800

    bg_video fix flashing loop

--- bg_video/assets/js/script.js
+++ bg_video/assets/js/script.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var i = 1;
+    var video1 = document.getElementById('video1');
+    var video2 = document.getElementById('video2');
+    var video3 = document.getElementById('video3');
     document.addEventListener('keypress', () => {
         if (i < 3) {
             i++;
             document.getElementById(`video${i}`).style['z-index'] = i;
             document.getElementById(`video${i}`).play();
+        } else {
+            video1.load();
+            video2.load();
+            video3.load();
+            video1.style['z-index'] = 1;
+            video2.style['z-index'] = 0;
+            video3.style['z-index'] = 0;
+            i = 1;
         }
     });
 });
