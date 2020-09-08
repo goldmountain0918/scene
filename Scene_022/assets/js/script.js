5f8e0e1c024ee6166bbc6f335861401956e3edd8
goldmountain0918 <goldmountain0918@gmail.com>
2020-09-08 12:19:22 +0800

    swap email_box

--- Scene_022/assets/js/script.js
+++ Scene_022/assets/js/script.js
@@ -5,11 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
     document.getElementById('button').addEventListener('click', () => {
         document.getElementById('launch').style['background-image'] = 'url("../Scene_022/assets/img/INITIATE.gif")';
-        document.getElementById('portrait_bg_loop').style['opacity'] = '1';
-        document.getElementById('portrait_bg_loop').play();
-        setTimeout(() => {
-            document.getElementById('portrait_bg_active').style['opacity'] = '1';
-            document.getElementById('portrait_bg_active').play();
-        }, 10000);
+        document.getElementById('portrait_bg_active').style['opacity'] = '1';
+        document.getElementById('portrait_bg_active').play();
     });
 });
